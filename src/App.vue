6a5fa1ff6a496f25cee295497fc79dcf7bba1692
Juan <damorjuancruz@gmail.com>
2021-06-27 11:48:24 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="my-8 m-auto w-full max-w-xl">
+		<Header />
+	</div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+	name: "App",
+	components: {
+		Header,
+	},
+};
+</script>
